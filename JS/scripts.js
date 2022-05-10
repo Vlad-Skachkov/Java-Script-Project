52f59c4a7bede40b6f635e80a720fe8d5f59d7a4
Vlad-Skachkov <103145025+Vlad-Skachkov@users.noreply.github.com>
2022-05-10 10:00:38 -0400

    First Commit

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello World');
+
+//Practice
+let favoriteFood = 'Pizza';
+let favoriteDrink = 'Sprite';
+let favoriteMeal = (favoriteDrink) + (' and ') + (favoriteFood);
+document.write(favoriteMeal);
+
+//First Commit to Gits
