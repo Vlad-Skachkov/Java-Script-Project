4c283cf583343fb8de743c0347aef3e7462bfc18
Vlad-Skachkov <103145025+Vlad-Skachkov@users.noreply.github.com>
2022-06-30 23:11:23 -0400

    New Project
    First 6 Pokemon, height and type

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,13 @@
-alert('Hello World');
 
-//Practice
-let favoriteFood = 'Pizza';
-let favoriteDrink = 'Sprite';
-let favoriteMeal = (favoriteDrink) + (' and ') + (favoriteFood);
-document.write(favoriteMeal);
 
 //First Commit to Gits
+
+
+let pokemonList = [
+    {name: 'Bulbasaur', type: ['grass', 'poison'], height:.7}, 
+    {name: 'Ivysaur', type: ['grass', 'poison'], height:1}, 
+    {name: 'Venusaur', type: ['grass', 'poison'], height:2},
+    {name: 'Charmander', type: 'fire', height:.6}, 
+    {name: 'Charmeleon', type: 'fire', height:1.1},
+    {name: 'Charizard', type: ['fire', 'flying'], height:1.7}
+];
